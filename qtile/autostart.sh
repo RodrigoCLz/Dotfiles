05c4b1cc921071c1dd2df7ff271e06792d7db2fd
RodrigoCLz <rcastillolaz@gmail.com>
2025-06-02 07:55:04 -0500

    autostart.sh corregido

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# wallpapers
-feh --bg-scale ~/Imágenes/wallpapers/steampunck_1.jpg
-# transparencia de terminal
-picom --backend glx --vsync -b
 # systray battery icon
 cbatticon -u 5 &
 # Deteccion de pantalla secundaria 
 xrandr --output HDMI-1-0 --mode 1920x1080 --rate 74.99 --right-of eDP-1
+# wallpapers
+feh --bg-scale ~/Imágenes/wallpapers/steampunck_1.jpg
+# transparencia de terminal
+picom --backend glx --vsync -b
